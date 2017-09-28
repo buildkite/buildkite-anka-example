f521148cf8a0c806f7557357143588a6c8066364
Lachlan Donald <lachlan@buildkite.com>
2017-09-28 16:25:59 +1000

    Use ephemeral containers in parallel

--- build.sh
+++ build.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "+++ Building app in $ANKA_IMAGE_NAME"
-anka run "$ANKA_IMAGE_NAME" xcodebuild build-for-testing \
+job_image_name="${ANKA_IMAGE_NAME}-${BUILDKITE_JOB_ID}"
+
+cleanup() {
+  echo "--- Cleaning up images"
+  anka delete --yes "$job_image_name"
+}
+
+echo "--- Cloning $ANKA_IMAGE_NAME to $job_image_name"
+anka clone "$ANKA_IMAGE_NAME" "$job_image_name"
+
+trap cleanup EXIT
+
+echo "+++ Building app in $job_image_name"
+anka run "$job_image_name" xcodebuild build-for-testing \
   -project "Buildkite Anka Demo.xcodeproj" \
   -scheme "Buildkite Anka Demo" \
   -destination "$XCODE_DESTINATION" \
--- test.sh
+++ test.sh
@@ -4,10 +4,23 @@ set -euo pipefail
 echo "--- Downloading build artifacts"
 buildkite-agent artifact download "build/**" .
 
+job_image_name="${ANKA_IMAGE_NAME}-${BUILDKITE_JOB_ID}"
+
+cleanup() {
+  echo "--- Cleaning up images"
+  anka delete --yes "$job_image_name"
+}
+
+echo "--- Cloning $ANKA_IMAGE_NAME to $job_image_name"
+anka clone "$ANKA_IMAGE_NAME" "$job_image_name"
+
+trap cleanup EXIT
+
 echo "+++ Running tests in $ANKA_IMAGE_NAME"
 anka run "$ANKA_IMAGE_NAME" xcodebuild test-without-building \
   -project "Buildkite Anka Demo.xcodeproj" \
   -scheme "Buildkite Anka Demo" \
   -sdk iphonesimulator \
   -destination "$XCODE_DESTINATION" \
-  -derivedDataPath "build"
+  -derivedDataPath "build" \
+  -only-testing "$1"
